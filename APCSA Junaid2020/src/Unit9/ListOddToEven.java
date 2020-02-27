aea7a79123aa53fb0de44a049d90a6f5538ac47a
ahmadj8162 <ahmadj8162@CA-SU-F106-06.sduhsd.lan>
2020-02-27 10:16:08 -0800

    Finished Unit9 Labs

--- APCSA Junaid2020/src/Unit9/ListOddToEven.java
+++ APCSA Junaid2020/src/Unit9/ListOddToEven.java
@@ -13,4 +13,30 @@ public class ListOddToEven
 	{
 		return false;
 	}
+	public static int distance(List<Integer> ray) {
+		Integer odd = null;
+		Integer even=null;
+		int i = 0;
+		
+		int count =0;
+		for (i=0;i<ray.size();i++) {
+			if(ray.get(i)%2==1) {
+				odd = ray.get(i);
+				break;
+			}
+			
+		}
+		for ( int y=i;y<ray.size();y++) {
+			if(ray.get(y)%2==0) {
+				even = ray.get(y);
+				count = y;
+			}
+		}
+		if(odd==null||even==null) {
+			return -1;
+		}
+		else {
+			return count-i;
+		}
+	}
 }
