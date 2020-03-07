d9711c6372a003b17fc6e003e5b8d419e54e6596
cootaca <53381246+cootaca@users.noreply.github.com>
2020-03-06 23:30:59 -0800

    Update ToyStoreRunner.java

--- APCSA Junaid2020/src/Unit10/ToyStoreRunner.java
+++ APCSA Junaid2020/src/Unit10/ToyStoreRunner.java
@@ -1,7 +1,7 @@
 package Unit10;
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
+// (c) A+ Computer Science
+// www.apluscompsci.com
+// Name - Junaid Ahmad
 
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -12,6 +12,10 @@ public class ToyStoreRunner
 {
 	public static void main( String args[] )
 	{
-
+		ToyStore6 sto = new ToyStore6();
+		System.out.println( sto );
+		sto.loadToys("sorry bat sorry sorry sorry train train teddy teddy ball ball" );
+		System.out.println( sto );	
+		System.out.println( "max == " + sto.getMostFrequentToy() );		
 	}
 }
